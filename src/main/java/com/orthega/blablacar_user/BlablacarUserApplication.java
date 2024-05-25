bc73013d1a3897757cdfa811465f4f934a133389
orthegavalentin <iravadu@gmail.com>
2024-05-25 10:51:12 +0200

    initial commit

---
+++ src/main/java/com/orthega/blablacar_user/BlablacarUserApplication.java
@@ -0,0 +1,13 @@
+package com.orthega.blablacar_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablacarUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlablacarUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orthega/blablacar_user/BlablacarUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orthega.blablacar_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlablacarUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
